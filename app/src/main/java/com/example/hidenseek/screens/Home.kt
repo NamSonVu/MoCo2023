b7050bcab9957454b135d3727ca1d87b62739547
Nam Son Vu <nam_son.vu@smail.th-koeln.de>
2023-06-03 02:53:04 +0200

    scrollable page

---
+++ app/src/main/java/com/example/hidenseek/screens/Home.kt
@@ -0,0 +1,29 @@
+package com.example.hidenseek.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    Column(modifier = Modifier
+        .padding(8.dp)){
+
+        Text(
+            text = "HOME",
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+
+
+
+    }
+
+}
